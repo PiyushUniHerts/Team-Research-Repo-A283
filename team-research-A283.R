61289525da69730e52e049ff269a260b3542c9b8
PiyushUniHerts <pm24abn@herts.ac.uk>
2024-11-21 18:05:12 +0000

    first commit r file

--- team-research-A283.R
+++ team-research-A283.R
@@ -1,1 +1,7 @@
-# this is the file we will be editing and uploading
+# fresh file 
+library(readxl)
+gfg_data = read_excel("length_of_hospital_stay.xlsx")
+gfg_data
+summary(gfg_data)
+mean(gfg_data$Value[gfg_data$SUBJECT == "ACUTE"])
+mean(gfg_data$Value[gfg_data$SUBJECT == "CHILDBIRTH"])
